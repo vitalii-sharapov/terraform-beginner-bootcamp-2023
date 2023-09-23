114cb270370953006139cabfcb16dc8c6727bf08
Vitalii Sharapov <vitalii.sharapov@proton.me>
2023-09-23 14:23:17 +0000

    provide example env for env var project root, provide good documentation about env vars

--- bin/install_terraform_cli.sh
+++ bin/install_terraform_cli.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+cd /workspace
+
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -13,3 +15,4 @@ sudo apt update -y
 sudo apt-get install terraform -y 
 terraform -install-autocomplete -y
 
+cd $PROJECT_ROOT
