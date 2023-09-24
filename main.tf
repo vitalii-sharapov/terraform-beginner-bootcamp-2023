6e43eb17a9ae0a5bc446fa74c759e19175e0696c
Vitalii Sharapov <90422092+vitalii-sharapov@users.noreply.github.com>
2023-09-24 11:11:17 -0600

    #11 install terrafrom provider, create s3 bucket. (#12)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
@@ -11,9 +15,22 @@ provider "random" {
   # Configuration options
 }
 
+provider "aws" {
+  # Configuration options
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower = true
+  upper = false
+  length           = 32
   special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name_id" {
