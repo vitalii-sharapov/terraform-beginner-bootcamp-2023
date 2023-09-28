debee508f6aaca238aecce52081ae67866f38c4f
Vitalii Sharapov <vitalii.sharapov@proton.me>
2023-09-28 03:54:54 +0000

    #21 added few tf files

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "Vitalii-Sharapov"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
@@ -38,8 +10,4 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
-
-output "random_bucket_name_id" {
-    value = random_string.bucket_name.result
 }
