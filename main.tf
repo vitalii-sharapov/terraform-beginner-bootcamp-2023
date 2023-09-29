96f25ed5d2446dd7c4f9f4fc94c8a2e3f1f35700
Vitalii Sharapov <vitalii.sharapov@proton.me>
2023-09-29 03:40:03 +0000

    #23 bucket should no longer use random provider

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length           = 32
-  special          = false
-}
-
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = random_string.bucket_name.result
-  
+  bucket = var.bucket_name
+
   tags = {
-    UserUuid        = var.user_uuid
+   UserUuid = var.user_uuid
   }
 
 }
