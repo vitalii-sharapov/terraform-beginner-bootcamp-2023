01fd0f99225533dc18f5a0332fd50b2229193b6d
Vitalii Sharapov <90422092+vitalii-sharapov@users.noreply.github.com>
2023-10-09 19:23:13 -0600

    #41 work in progress setting up base for creating a custom terraform provider (#42)

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   #cloud {
   #  organization = "Vitalii-Sharapov"
   #
@@ -8,12 +14,29 @@ terraform {
   #}
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#  assets_path = var.assets_path
+#}
+
+resource "terratowns_home" "home" {
+  name = "Play Factorio!"
+  description = <<DESCRIPTION
+Factorio is a construction and management simulation game developed and published by Czech studio Wube Software.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
 }
